91c9b5f5d949c36252b996c5b466fddbddf64159
Al-3x <166305327+Al-3x@users.noreply.github.com>
2024-05-09 17:46:18 -0300

    Update LoginActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/LoginActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/LoginActivity.kt
@@ -17,13 +17,18 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonEntrar.setOnClickListener {
+
+            // Aqui, estamos obtendo os valores dos campos de entrada de text
+            //e armazenando-os nas variáveis 'username' e 'password'
             val username = binding.editUsername.text.toString().trim()
             val password = binding.editPassword.text.toString().trim()
 
             if (username.equals("Alex") && password.equals("2")) {
                 val i = Intent(this, MainActivity::class.java)
                 i.putExtra("username", username)
+                // Aqui começa a atividade
                 startActivity(i)
+                // Aqui termina a atividade
                 finish()
             } else {
                 Toast.makeText(applicationContext, "Errou", Toast.LENGTH_LONG).show()
